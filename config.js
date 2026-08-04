3cd4123156d52973b66a430cef870da1e49be6a9
uybanmattranthanhkhe-glitch <uybanmattranthanhkhe@gmail.com>
2026-08-04 18:13:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 // URL Apps Script sau khi deploy Code.gs (chỉ dành cho tổng hợp)
 // Lấy tại: script.google.com → Triển khai → Triển khai mới → Ứng dụng web
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzUo91VCFfBws-1bzxHbhj29gfKdkGrdM683de-SHqMuzcBNpjBnP-YPnFHjKT3-Oay/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwe1i71ClxjZ4XcdEhQeYeSA2g5YrSkFFF9P5nXYfKk7ek3nams9214U7IQzG3lhlCm/exec';
 
 // Link thư mục Google Drive chứa file góp ý (mặc định)
 // Dán link thư mục vào đây — app sẽ tự dùng
